621be6874565f7b673e574c4acd6c687b9cb6b69
mikeblackdev <mikeblackdev@gmail.com>
2023-10-25 22:23:56 -0700

    Add styling to navigation bar

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { NavigationStart, Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'smart';
+  public navStyle: any;
+
+  constructor(private router: Router) {
+    router.events.subscribe((event) => {
+      if(event instanceof NavigationStart) {
+        this.navStyle = { 'background-color': event.url === '/' ? 'white' : 'black', 'color': event.url === '/' ? 'black' : 'white' };
+      }
+    });
+  }
 }
